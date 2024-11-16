c9f220c6ffd981ea35ade3d183422d9b149c2927
CheonMinJae <cmjcmj1011@gmail.com>
2024-11-16 15:46:47 +0900

    first commit

---
+++ src/test/java/com/gdsc/game/ApplicationTests.java
@@ -0,0 +1,12 @@
+package com.gdsc.game;
+
+import org.junit.jupiter.api.Test;
+
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("hello test");
+    }
+
+}
