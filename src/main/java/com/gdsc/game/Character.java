82aa0167c4e4245e8a82718cdabf696898dee71b
mirupio <162397816+mirupio@users.noreply.github.com>
2024-11-21 14:55:25 +0900

    create Character

---
+++ src/main/java/com/gdsc/game/Character.java
@@ -0,0 +1,18 @@
+package com.gdsc.game;
+
+import java.util.Random;
+
+public class Character {
+    private String name;
+    private int health;
+    private int mana;
+    private Skill[] skills;
+
+    // 생성자
+    public Character(String name, int health, int mana, Skill[] skills) {
+        this.name = name;
+        this.health = health;
+        this.mana = mana;
+        this.skills = skills;
+    }
+}
