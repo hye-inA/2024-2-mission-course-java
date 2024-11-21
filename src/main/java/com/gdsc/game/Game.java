4e8ff724e80c1963a5be1e2528751c61498f9ff6
mirupio <162397816+mirupio@users.noreply.github.com>
2024-11-21 15:06:52 +0900

    turn progress logic

--- src/main/java/com/gdsc/game/Game.java
+++ src/main/java/com/gdsc/game/Game.java
@@ -19,4 +19,65 @@ public class Game {
         this.currentPlayer = player1;
         this.opponent = player2;
     }
+
+    // 턴 진행 로직
+    public void nextTurn() {
+        if(isGameOver()){
+            return;
+        }
+
+        printStatus();
+
+        // 플레이어 행동 선택
+        Scanner scanner = new Scanner(System.in);
+        int action = scanner.nextInt();
+
+        int damage = 0;
+        switch(action){
+            // 공격
+            case 1:
+                damage = currentPlayer.attack();
+                opponent.decreaseHealth(damage);
+                break;
+
+            // 방어
+            case 2:
+                damage = opponent.attack() - currentPlayer.defend();
+                if (damage < 0) damage = 0;
+                currentPlayer.decreaseHealth(damage);
+                break;
+
+            // 스킬 사용
+            default:
+                Skill[] skills = currentPlayer.getSkills();
+                int skill = action - 3;
+                damage = currentPlayer.useSkill(skills[skill]);
+                if (damage > 0){
+                    opponent.decreaseHealth(damage);
+                }
+                else{
+                    System.out.println("스킬 사용 불가");
+                }
+                break;
+        }
+        reduceCooldowns();
+        swapPlayers();
+        turnCount++;
+    }
+
+    // 쿨다운 적용
+    private void reduceCooldowns(){
+        for(Skill skill : currentPlayer.getSkills()){
+            skill.reduceCooldown();
+        }
+    }
+
+    // 플레이어 교체
+    private void swapPlayers(){
+        Character temp = currentPlayer;
+        currentPlayer = opponent;
+        opponent = temp;
+    }
+
+
 }
